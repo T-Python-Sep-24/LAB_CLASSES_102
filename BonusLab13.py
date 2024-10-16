842d0ed42810f1043e05cd7055570a9c4c546853
KhuloodGhazi <khulood.gmo@gmail.com>
2024-10-17 00:30:14 +0300

    Last Modify

---
+++ BonusLab13.py
@@ -0,0 +1,70 @@
+#Bonus Lab 13
+
+import pickle
+from Lab13 import BankAccount
+
+class AccountManager:
+    def __init__(self):
+        self.user_accounts = []
+
+#Add Account
+    def add_account(self, bank_account: BankAccount):
+        self.user_accounts.append(bank_account)
+        print("Bank Account Added Successfully.")
+
+
+#Display Account   
+    def display_accounts(self):
+        if not self.user_accounts:
+            print("No accounts found.")
+        for account in self.user_accounts:
+            print(account)
+
+
+#Search Account
+def search_accounts(self, account_number):
+        for account in self.user_accounts:
+            if account.account_number == account_number:
+                print(account)
+                return account
+            
+        else:    
+            print("Account is Not Found")
+            return None
+        
+
+#Delete Account
+def delete_account(self, account_number):
+        account = self.search_accounts(account_number)
+        if account:
+            self.user_accounts.remove(account)
+            print("Account Deleted Successfully")
+
+        else:
+            print("The Account Could Not be Deleted")
+
+
+#Save Account with pickle
+def save_account(self, filename:str):
+    try:
+        with open(filename, 'wb') as file:
+            pickle.dump(self.user_accounts, file)
+            print("Accounts Saved")
+    
+    except Exception as e:
+        print(f"Error Saving File: {e}")
+
+
+#Load Account with pickle
+def load_account(self, filename: str):
+    try:
+        with open(filename, 'rb') as file:
+            self.user_accounts = pickle.load(file)
+            print("Accounts Loaded")
+            
+    except FileNotFoundError:
+        print("File Not Found")
+        
+    except Exception as e:
+        print(f"Error Loading File: {e}")        
+
--- main.py
+++ main.py
@@ -1,5 +1,69 @@
 from Lab13 import BankAccount
+from BonusLab13 import AccountManager
 
+#This is for Bonus Lab 13
+def main():
+
+    manager = AccountManager()
+    filename = "accounts_data.pkl"
+    manager.load_account(filename)
+
+    print("Welcom To Your Bank Account Manager")
+    print("*" * 20)
+
+    while True:
+        
+        user_choice = input("Please Enter Your Choice: \n1. Add New Account \n2. Display All Accounts \n3. Search for an Account \n4. Delete an Account \n5. Save Accounts to File \n6. Load Accounts from File \n7. Exit \n")
+
+        if user_choice == "1":
+            name = input("Enter The Name of the Account: ")
+            balance = float(input("Enter Initial Balance: "))
+            new_account = BankAccount(name, balance)
+            manager.add_account(new_account)
+
+
+        elif user_choice == "2":
+            manager.display_accounts()    
+
+
+        elif user_choice == "3":
+            account_number = int(input("Enter The Account Number You Want To Search: "))
+            manager.search_accounts(account_number)
+
+
+        elif user_choice == "4":
+            account_number = int(input("Enter The Account Number You Want To Delete: "))
+            manager.delete_account(account_number)
+
+
+        elif user_choice == "5":
+            filename = input("Enter The File Name to Save Accounts: ") 
+            manager.save_account(filename)
+
+
+        elif user_choice == "6":
+            filename = input("Enter The File Name to Load Accounts: ")
+            manager.load_account(filename)
+
+
+        elif user_choice == "7":
+            print("Exit Bank Manager")
+            break
+
+
+        else:
+            print("Something Worng, Please Try Again") 
+
+
+
+if __name__ == "__main__":
+    main()                   
+
+
+                            
+
+
+#This is for Lab 13
 user1 = BankAccount("Khulood", 500)
 
 user1.deposit()
