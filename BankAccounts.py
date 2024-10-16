8f81b6144aa7b909b89259722ec5eef87aee8496
Fahad <fahad.alabdulkarim5@gmail.com>
2024-10-16 23:17:17 +0300

    LAB_CLASSES_102+bouns

--- BankAccounts.py
+++ BankAccounts.py
@@ -1,6 +1,6 @@
 import random
 import os
-
+import pickle
 
 class BankAccount():
     number_list = []
@@ -9,24 +9,32 @@ class BankAccount():
         self.account_holder = account_holder
         self.balance = balance
         self.account_number = self.__account_number()
+        
+
 
     @classmethod
-    def load_numbers(cls, filename: str):
-        if os.path.exists(filename):
-            with open(filename, 'r') as file:
-                for line in file:
-                    cls.number_list.append(int(line.strip()))
-        else:
-            open(filename, 'w').close()
+    def save_acc_list(self, filename: str):
+        """saves the account numbers to a file"""
+        with open(filename, 'w') as file:  
+            for account_number in self.number_list:
+                file.write(f"{account_number}\n")  
+
 
     @classmethod
-    def save_numbers(cls, filename: str):
-        with open(filename, 'w') as file:
-            for number in cls.number_list:
-                file.write(f"{number}\n")
+    def load_from_file(self, filename: str):
+        """loads account numbers from a file into number_list using this list to check for repeated account number"""
+        self.number_list = []  
+        with open(filename, 'r') as file:
+            for line in file:
+                account_number = int(line.strip())  
+                if account_number not in self.number_list:  
+                    self.number_list.append(account_number)
+    
 
 
     def __account_number(self):
+        """this function is used to generate account numebrs
+    """
         
         while True:
             random_account_number = random.randint(1000000000, 9999999999)
@@ -39,27 +47,12 @@ class BankAccount():
 
 
     def deposit(self, amount:int):
-        #self.account_holder = input("enter account name: ")
-        #self.account_number = input("enter account number:")
-        #if self.account_number in BankAccount.number_list:
-            #amount = input("enter amount to deposit: ")
         self.balance += amount
-            #return (f"account balance {self.balance}")
-        #else:
-            #print("account not found")
 
 
     def withdraw(self, amount):
-        #self.account_holder = input("enter account name: ")
-        #self.account_number = input("enter account number:")  
-        #if self.account_number in BankAccount.number_list:
-            #amount = input("enter amount to deposit: ")      
-            #if amount <= self.balance:
             
         self.balance -= amount
-                #return (f"account balance: {self.balance}")
-            #else:
-                #print("balance is not enough")
 
 
     def get_balance(self):
@@ -74,9 +67,100 @@ class BankAccount():
         print(f"account number is: {self.account_number}")
     
     def __str__(self):
+        """this function is used to format objects
+    """
         return f"Account holder: {self.account_holder}, Account number: {self.account_number}, Balance: {self.balance}"
 
 
-    def save_file(self, filename: str):
+    def save_to_file(self, filename: str):
+        """this function is used to save accounts to txt file
+    """
         with open(filename, 'a') as file:  
             file.write(f"{self.account_holder},{self.account_number},{self.balance}\n")
+
+
+    @classmethod
+    def load_file(self, filename: str):
+        accounts = []
+        with open(filename, 'r') as file:
+            for line in file:  
+                account_holder, account_number, balance = line.strip().split(',')  
+                account = BankAccount(account_holder, int(balance))  
+                account.account_number = int(account_number)  
+                accounts.append(account)  
+        return accounts 
+    
+        
+
+class AccountManager(BankAccount):
+    filename = 'Bank_Accounts.pkl'
+    def __init__(self, filename) -> None:
+        self.filename = filename
+        if not os.path.exists(self.filename):
+            self.accounts = []  
+            self.save_to_pickle()  
+        else:
+            self.accounts = self.load_from_pickle()
+        
+    
+    def add_account(self, bank_account: BankAccount =None):
+        """this function is used to add account to pkl file
+    """
+        account_holder = input("enter account holder name: ")
+        balance = input("enter balance for account: ")
+        bank_account = BankAccount(account_holder, balance)
+        
+        self.accounts.append(bank_account)
+        bank_account.save_to_file(self.filename)
+
+    
+    def display_accounts(self):
+        if not self.accounts:
+            print("No accounts found.")
+        for account in self.accounts:
+            print(account)
+    
+
+    def search_accounts(self):
+        acc_number = input("Enter account number you want to find: ")
+        found = False
+        for account in self.accounts:
+            if account.account_number == int(acc_number):  
+                print(account)
+                found = True
+                break
+    
+        if not found:
+            print("Account not found.")
+    
+
+    def delete_account(self, account_number = 0):
+        
+        acc_number = input("enter account number you want to delete: ")
+        acc_del = None
+        for account in self.accounts:
+            if account.account_number == int(acc_number):
+                acc_del = account
+                break
+        if acc_del:
+            self.accounts.remove(acc_del)
+            self.save_to_pickle()
+        else:
+            print("account not found")
+   
+    def save_to_pickle(self):
+        with open(self.filename, 'wb') as file:
+            pickle.dump(self.accounts, file)
+
+    def load_from_pickle(self):
+        try:
+            with open(self.filename, 'rb') as file:
+                return pickle.load(file)
+        except (EOFError, FileNotFoundError):
+            return []
+
+
+
+
+
+    
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from BankAccounts import BankAccount
+from BankAccounts import BankAccount, AccountManager 
 
-BankAccount.load_numbers("bank_accounts_numbers.txt")
+BankAccount.load_from_file("bank_accounts_numbers.txt")
+BankAccount.load_file("Bank_Accounts.txt")
 
 person1 = BankAccount("fahad", 500)
+person2 = BankAccount("ahmed", 200)
 print(person1)
 
-deposit = person1.deposit( 500)
-print(person1)
+person1.deposit( 500)
 
-withdraw = person1.withdraw(200)
-print(person1)
+
+person1.withdraw(200)
+
 
 person1.get_balance()
 
@@ -20,5 +22,38 @@ person1.get_account_holder()
 person1.get_account_number()
 
 
-person1.save_file("Bank_Accounts.txt")
-BankAccount.save_numbers("bank_accounts_numbers.txt")
+person1.save_to_file("Bank_Accounts.txt")
+person2.save_to_file("Bank_Accounts.txt")
+BankAccount.save_acc_list("bank_accounts_numbers.txt")
+
+
+
+def main():
+    """
+    this function is used to ask the user to modify bank
+    """
+    am = AccountManager("Bank_Accounts.pkl")
+    while True:
+        print("\n1. Add Account")
+        print("2. Display Accounts")
+        print("3. Search Account")
+        print("4. Delete Account")
+        print("5. Exit")
+
+        choice = input("Select an option (1-5): ")
+
+        if choice == '1':
+            am.add_account()
+        elif choice == '2':
+            am.display_accounts()
+        elif choice == '3':
+            am.search_accounts()
+        elif choice == '4':
+            am.delete_account()
+        elif choice == '5':
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid choice, please try again.")
+    am.save_to_pickle()
+main()
